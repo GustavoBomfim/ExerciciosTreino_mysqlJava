e912c3b0010fe1221584e34f52173465cbbcf8a4
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-19 15:25:49 -0300

    exercicio_0

---
+++ src/Program.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Program {
+    public static void main(String[] args){
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "DELETE FROM department"
+                    + "WHERE "
+                    + "Id = ?");
+
+            st.setInt(1,5);
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        }
+        catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+    }
+}
